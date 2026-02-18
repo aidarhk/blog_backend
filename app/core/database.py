d3e86a2288a7cde34933242983087fc7bbeb2fe0
VovaLi <jektrekpekvek@protonmail.ch>
2026-02-18 20:10:31 +0300

    точно последнее

--- app/core/database.py
+++ app/core/database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},  # ✅ Для Docker
+    connect_args={"check_same_thread": False},
     echo=settings.DEBUG
 )
 
